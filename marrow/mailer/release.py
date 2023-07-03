db1bcf43098b8cb485422d592528de884eb00ac2
Démian Janssen <demian@checksec.com>
2023-07-03 15:20:05 +0200

    Refactor the versioning

--- marrow/mailer/release.py
+++ marrow/mailer/release.py
@@ -4,11 +4,28 @@
 
 from collections import namedtuple
 
+VersionInfo = namedtuple(
+	'Version', (
+		'major',
+		'minor',
+		'micro',
+		'releaselevel',
+		'serial',
+	),
+)
 
-version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(4, 1, 3, 'dev', 0)
-version = ".".join([str(i) for i in version_info[:3]]) + ((version_info.releaselevel[0] + str(version_info.serial)) if version_info.releaselevel != 'final' else '')
+AuthorInfo = namedtuple(
+	'Author', (
+		'name',
+		'email',
+	),
+)
 
-author = namedtuple('Author', ['name', 'email'])("Alice Bevan-McGregor", 'alice@gothcandy.com')
+info = VersionInfo(4, 1, 3, 'dev', 0)
+release = f"{info.releaselevel[0]}{info.serial}" if info.releaselevel != "final" else ""
+version = f"{info.major}.{info.minor}.{info.micro}{release}"
+
+author = AuthorInfo("Alice Bevan-McGregor", 'alice@gothcandy.com')
 
 description = "A light-weight modular mail delivery framework for Python 2.7+, 3.3+, Pypy, and Pypy3."
-url = 'https://github.com/marrow/mailer/'
+url = "https://github.com/checksecltd/mailer"
