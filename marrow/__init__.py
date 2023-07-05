6990576d59e9ed54cdc8132e758e6cf97d60c7a6
Démian Janssen <demian@checksec.com>
2023-07-05 10:00:52 +0200

    Expose functionality from the top level module

---
+++ marrow/__init__.py
@@ -0,0 +1,16 @@
+from marrow.mailer import Mailer, Delivery
+from marrow.mailer.message import Message
+from marrow.mailer.address import (
+    Address,
+    AddressList,
+    AutoConverter,
+)
+
+__all__ = [
+    "Delivery",
+    "Mailer",
+    "Message",
+    "Address",
+    "AddressList",
+    "AutoConverter",
+]
